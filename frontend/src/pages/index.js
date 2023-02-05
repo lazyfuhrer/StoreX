55332e5e694ebad1d6071fe9532a39cb8b5d4c7a
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-02-05 21:44:33 +0530

    created components

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,15 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+//import styles from '@/styles/Home.module.css'
 import { Text } from '@chakra-ui/react'
+import FileUpload from '@/components/FileUpload'
+import Modal from '@/components/Modal'
+import Display from '@/components/Display'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      
+      <FileUpload/>
+      <Modal/>
+      <Display/>
     </>
   )
 }
